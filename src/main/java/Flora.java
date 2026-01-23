b234d85ba21d99e468a42782210fa6b273afba01
memoflora <fathmubina09@gmail.com>
2026-01-23 17:21:08 +0800

    Create skeletal version of Flora

---
+++ src/main/java/Flora.java
@@ -0,0 +1,11 @@
+public class Flora {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hi there! Flora here.\nAsk me anything!");
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Talk to you later—bye!");
+
+        System.out.println("____________________________________________________________");
+    }
+}
