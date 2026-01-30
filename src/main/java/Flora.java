bd5e86e81ac73115253148393a2f5b6a58f569a3
memoflora <fathmubina09@gmail.com>
2026-01-30 11:08:29 +0800

    Make output messages into string variables

--- src/main/java/Flora.java
+++ src/main/java/Flora.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
 public class Flora {
+    private static final String line = "    ____________________________________________________________";
+    private static final String indent = "     ";
+    private static final String greeting = indent + "Hi there! Flora here.\n" + indent + "Ask me anything!";
+    private static final String farewell = indent + "Talk to you later—bye!";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input;
 
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Hi there! Flora here.\n     Ask me anything!");
+        System.out.println(line);
+        System.out.println(greeting);
 
         while (true) {
-            System.out.println("    ____________________________________________________________\n");
+            System.out.println(line);
+            System.out.println();
+
             input = sc.nextLine();
             System.out.println(input);
 
-            System.out.println("    ____________________________________________________________");
+            System.out.println(line);
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println("     Talk to you later—bye!");
+                System.out.println(farewell);
                 break;
             }
 
-            System.out.print("     ");
-            System.out.println(input);
+            System.out.println(indent + input);
         }
 
-        System.out.println("    ____________________________________________________________");
+        System.out.println(line);
     }
 }
