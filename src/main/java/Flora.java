2c18fc94406e5546be224d6c6de9433889a413c6
memoflora <fathmubina09@gmail.com>
2026-01-30 13:08:58 +0800

    Add mark and unmark task feature

--- src/main/java/Flora.java
+++ src/main/java/Flora.java
@@ -6,22 +6,67 @@ public class Flora {
     private static final String greeting = indent + "Hi there! Flora here.\n" + indent + "Ask me anything!";
     private static final String farewell = indent + "Talk to you later—bye!";
 
-    private final String[] tasks;
+    private final Task[] tasks;
     private int taskCount;
 
     public Flora() {
-        this.tasks = new String[100];
+        this.tasks = new Task[100];
         this.taskCount = 0;
     }
 
     public void listTasks() {
+        System.out.println(indent + "Here are the tasks in your list:");
         for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            System.out.println(indent + (i + 1) + ". " + tasks[i].toString());
         }
     }
 
-    public void addTask(String task) {
-        tasks[taskCount++] = task;
+    public void addTask(String input) {
+        tasks[taskCount++] = new Task(input);
+        System.out.println(indent + "added: " + input);
+    }
+
+    public void markTask(int index) {
+        Task task = tasks[index - 1];
+        task.markAsDone();
+
+        System.out.println(indent + "Nice! I've marked this task as done:");
+        System.out.println(indent + "  " + task.toString());
+    }
+
+    public void unmarkTask(int index) {
+        Task task = tasks[index - 1];
+        task.markAsNotDone();
+
+        System.out.println(indent + "OK, I've marked this task as not done yet:");
+        System.out.println(indent + "  " + task.toString());
+    }
+
+    private static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
+        public void markAsDone() {
+            isDone = true;
+        }
+
+        public void markAsNotDone() {
+            isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + getStatusIcon() + "] " + description;
+        }
     }
 
     public static void main(String[] args) {
@@ -39,9 +84,24 @@ public class Flora {
             System.out.println();
 
             input = sc.nextLine();
+            String[] parts = input.split(" ");
+            String command = parts[0];
+
             System.out.println(line);
 
-            switch (input.toLowerCase()) {
+            switch (command.toLowerCase()) {
+                case "mark": {
+                    int taskIndex = Integer.parseInt(parts[1]);
+                    flora.markTask(taskIndex);
+                    break;
+                }
+
+                case "unmark": {
+                    int taskIndex = Integer.parseInt(parts[1]);
+                    flora.unmarkTask(taskIndex);
+                    break;
+                }
+
                 case "list":
                     flora.listTasks();
                     break;
@@ -50,8 +110,7 @@ public class Flora {
                     exit = true;
                     break;
                 default:
-                    flora.addTask(input);
-                    System.out.println("added: " + input);
+                    flora.addTask(command);
             }
         }
 
