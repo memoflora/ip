15f36d1dd2f5a38f219a9e46e6dd1933527a43ab
memoflora <fathmubina09@gmail.com>
2026-01-30 12:17:04 +0800

    Add tasks feature with add and list

--- src/main/java/Flora.java
+++ src/main/java/Flora.java
@@ -6,26 +6,53 @@ public class Flora {
     private static final String greeting = indent + "Hi there! Flora here.\n" + indent + "Ask me anything!";
     private static final String farewell = indent + "Talk to you later—bye!";
 
+    private final String[] tasks;
+    private int taskCount;
+
+    public Flora() {
+        this.tasks = new String[100];
+        this.taskCount = 0;
+    }
+
+    public void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    public void addTask(String task) {
+        tasks[taskCount++] = task;
+    }
+
     public static void main(String[] args) {
+        Flora flora = new Flora();
+
         Scanner sc = new Scanner(System.in);
         String input;
 
         System.out.println(line);
         System.out.println(greeting);
 
-        while (true) {
+        boolean exit = false;
+        while (!exit) {
             System.out.println(line);
             System.out.println();
 
             input = sc.nextLine();
             System.out.println(line);
 
-            if (input.equalsIgnoreCase("bye")) {
-                System.out.println(farewell);
-                break;
+            switch (input.toLowerCase()) {
+                case "list":
+                    flora.listTasks();
+                    break;
+                case "bye":
+                    System.out.println(farewell);
+                    exit = true;
+                    break;
+                default:
+                    flora.addTask(input);
+                    System.out.println("added: " + input);
             }
-
-            System.out.println(indent + input);
         }
 
         System.out.println(line);
