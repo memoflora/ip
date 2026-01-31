90afbb2a391b946783b7305fee26ebbab059ca56
memoflora <fathmubina09@gmail.com>
2026-01-31 16:39:38 +0800

    Make todo, deadline, and event feature

--- src/main/java/Flora.java
+++ src/main/java/Flora.java
@@ -17,13 +17,39 @@ public class Flora {
     public void listTasks() {
         System.out.println(indent + "Here are the tasks in your list:");
         for (int i = 0; i < taskCount; i++) {
-            System.out.println(indent + (i + 1) + ". " + tasks[i].toString());
+            System.out.println(indent + (i + 1) + "." + tasks[i]);
         }
     }
 
-    public void addTask(String input) {
-        tasks[taskCount++] = new Task(input);
-        System.out.println(indent + "added: " + input);
+    public void addTodo(String taskDesc) {
+        System.out.println(indent + "Got it. I've added this task:");
+
+        Todo todo = new Todo(taskDesc);
+        tasks[taskCount++] = todo;
+
+        System.out.println(indent + "  " + todo);
+        System.out.println(indent + "Now you have " + taskCount + " task" + (taskCount > 1 ? "s" : "") + " in the list.");
+    }
+
+    public void addDeadline(String taskDesc, String dueDate) {
+        System.out.println(indent + "Got it. I've added this task:");
+
+        Deadline deadline = new Deadline(taskDesc, dueDate);
+        tasks[taskCount++] = deadline;
+
+        System.out.println(indent + "  " + deadline);
+        System.out.println(indent + "Now you have " + taskCount + " task" + (taskCount > 1 ? "s" : "") + " in the list.");
+
+    }
+
+    public void addEvent(String taskDesc, String startTime, String endTime) {
+        System.out.println(indent + "Got it. I've added this task:");
+
+        Event event = new Event(taskDesc, startTime, endTime);
+        tasks[taskCount++] = event;
+
+        System.out.println(indent + "  " + event);
+        System.out.println(indent + "Now you have " + taskCount + " task" + (taskCount > 1 ? "s" : "") + " in the list.");
     }
 
     public void markTask(int index) {
@@ -31,7 +57,7 @@ public class Flora {
         task.markAsDone();
 
         System.out.println(indent + "Nice! I've marked this task as done:");
-        System.out.println(indent + "  " + task.toString());
+        System.out.println(indent + "  " + task);
     }
 
     public void unmarkTask(int index) {
@@ -39,34 +65,7 @@ public class Flora {
         task.markAsNotDone();
 
         System.out.println(indent + "OK, I've marked this task as not done yet:");
-        System.out.println(indent + "  " + task.toString());
-    }
-
-    private static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " ");
-        }
-
-        public void markAsDone() {
-            isDone = true;
-        }
-
-        public void markAsNotDone() {
-            isDone = false;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + getStatusIcon() + "] " + description;
-        }
+        System.out.println(indent + "  " + task);
     }
 
     public static void main(String[] args) {
@@ -84,21 +83,56 @@ public class Flora {
             System.out.println();
 
             input = sc.nextLine();
-            String[] parts = input.split(" ");
-            String command = parts[0];
+
+            int firstSpaceIndex = input.indexOf(" ");
+            String command;
+
+            if (firstSpaceIndex != -1) {
+                command = input.substring(0, firstSpaceIndex);
+            } else {
+                command = input;
+            }
 
             System.out.println(line);
 
             switch (command.toLowerCase()) {
+                case "todo": {
+                    String taskDesc = input.substring(firstSpaceIndex + 1);
+                    flora.addTodo(taskDesc);
+                    break;
+                }
+
+                case "deadline" : {
+                    int byIndex = input.indexOf("/by");
+
+                    String taskDesc = input.substring(firstSpaceIndex + 1, byIndex - 1);
+                    String dueDate = input.substring(byIndex + 4);
+
+                    flora.addDeadline(taskDesc, dueDate);
+                    break;
+                }
+
+                case "event" : {
+                    int fromIndex = input.indexOf("/from");
+                    int toIndex = input.indexOf("/to");
+
+                    String taskDesc = input.substring(firstSpaceIndex + 1, fromIndex - 1);
+                    String startTime = input.substring(fromIndex + 6, toIndex - 1);
+                    String endTime = input.substring(toIndex + 4);
+
+                    flora.addEvent(taskDesc, startTime, endTime);
+                    break;
+                }
+
                 case "mark": {
-                    int taskIndex = Integer.parseInt(parts[1]);
-                    flora.markTask(taskIndex);
+                    String taskIndex = input.substring(firstSpaceIndex + 1);
+                    flora.markTask(Integer.parseInt(taskIndex));
                     break;
                 }
 
                 case "unmark": {
-                    int taskIndex = Integer.parseInt(parts[1]);
-                    flora.unmarkTask(taskIndex);
+                    String taskIndex = input.substring(firstSpaceIndex + 1);
+                    flora.unmarkTask(Integer.parseInt(taskIndex));
                     break;
                 }
 
@@ -110,7 +144,7 @@ public class Flora {
                     exit = true;
                     break;
                 default:
-                    flora.addTask(command);
+                    System.out.println(input);
             }
         }
 
