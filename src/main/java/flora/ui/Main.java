762df1ca4ae0df539c8bbb04cac4a10a7d96b150
memoflora <fathmubina09@gmail.com>
2026-02-20 23:08:44 +0800

    Add GUI window title

--- src/main/java/flora/ui/Main.java
+++ src/main/java/flora/ui/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Flora");
 
             stage.setMinHeight(220);
             stage.setMinWidth(417);
